160f52e8625063ad2cf6ba1882dec212e5ec7789
ltvnkvtl <vital.litovk@gmail.com>
2021-11-23 00:05:35 +0300

    refactor(hw-6): change main index.ts file, make it pretty

--- hw-6-jwt-and-validation/src/index.ts
+++ hw-6-jwt-and-validation/src/index.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose';
 import express from 'express';
+import { config } from 'dotenv';
+import cookieParser from 'cookie-parser';
+import { serve, setup } from 'swagger-ui-express';
 
 import * as swaggerDocument from './swagger.json';
 import userRouter from './routes/userRouter';
-import {errorHandler} from "./middleware/error-middleware";
+import { errorMiddleware } from "./middleware/error-middleware";
 
-const dotenv = require('dotenv').config();
-const cookieParser = require('cookie-parser');
+config(); // .env
 
-const swaggerUi = require('swagger-ui-express');
-const DB_URL = `mongodb+srv://rd-hw-5-admin:${process.env.MONGO_PASSWORD}@cluster0.9sspa.mongodb.net/UsersAndTokens?retryWrites=true&w=majority`;
-console.log(DB_URL)
+const port = process.env.PORT || 3030;
+
 const app = express();
 app.use(
     express.urlencoded({
@@ -19,15 +20,14 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', serve, setup(swaggerDocument));
 app.use('/api', userRouter);
-app.use(errorHandler)
+app.use(errorMiddleware)
 
-const port = 3030;
 
 function startApp(): void {
     try {
-        mongoose.connect(DB_URL, (err) => {
+        mongoose.connect(`${process.env.DB_URL}`, (err) => {
             if (err) return console.log(err);
             app.listen(port, () => console.log(`Server started on port ${port}`));
         });
--- hw-6-jwt-and-validation/src/middleware/error-middleware.ts
+++ hw-6-jwt-and-validation/src/middleware/error-middleware.ts
@@ -1,7 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 import ApiError from "../exceptions/ApiErrors";
 
-export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+export const errorMiddleware = (err: any, req: Request, res: Response, next: NextFunction) => {
   console.log(err);
 
   if (err instanceof ApiError) {
